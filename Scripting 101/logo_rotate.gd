34fd587dd06c8a7fa12113291dde19019b32bf8a
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-07-28 10:26:19 -0400

    In the process of learning about signals. Like buttons and triggers that trigger a set of code or scripts

--- Scripting 101/logo_rotate.gd
+++ Scripting 101/logo_rotate.gd
@@ -1,11 +1,14 @@
 extends Sprite2D
 
-var speed = 400
+##variable used to determine the speed of the player-controlled asset
+var speed = 400 
+##
 var angular_speed = PI
 
 func _process(delta):
 	rotation += angular_speed * delta
 	var velocity = Vector2.UP.rotated(rotation) * speed
-	
 	position += velocity * delta
 
+func _on_button_pressed():
+	set_process(not is_processing())
