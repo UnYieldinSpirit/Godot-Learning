1629c55cbd8560ea07ab75b420e582981fa50ff1
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-07-28 22:37:06 -0400

    Learned about the importance of signals. Still a bit confused though...

--- Scripting 101/logo_rotate.gd
+++ Scripting 101/logo_rotate.gd
@@ -2,7 +2,6 @@ extends Sprite2D
 
 ##variable used to determine the speed of the player-controlled asset
 var speed = 400 
-##
 var angular_speed = PI
 
 func _process(delta):
@@ -12,3 +11,10 @@ func _process(delta):
 
 func _on_button_pressed():
 	set_process(not is_processing())
+
+func _ready():
+	var timer = get_node("Timer") ##timer can be used for skill cooldowns
+	timer.timeout.connect(_on_timer_timeout) ##after timer timesout it calls a function
+	
+func _on_timer_timeout(): ##function called after timeout: invisibility
+	visible = not visible
